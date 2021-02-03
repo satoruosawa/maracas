9a2bf8f8ed39d33d019dfc9b261b49312c3db269
Satoru Osawa <osawa@takram.com>
2021-02-03 23:38:29 +0900

    tilt

--- src/main.cpp
+++ src/main.cpp
@@ -19,21 +19,31 @@ void loop() {
     float accY = 0.0;
     float accZ = 0.0;
     M5.IMU.getAccelData(&accX, &accY, &accZ);
-    float absAcc = abs(accX) + abs(accY) + abs(accZ);
+    float absAcc = abs(accX);
+
+    static float absAccAvg = absAcc;
+    float avgCoef = 0.01;
+    absAccAvg = absAccAvg * (1 - avgCoef) + absAcc * avgCoef;
+
+    unsigned long interval = 100000;
+    static unsigned long prevMicros = micros();
+    if (micros() < prevMicros + interval) return;
+    prevMicros = micros();
     float calcCoef = 1000.0;
     float fromMin = 0.0;
-    float fromMax = 5.0;
+    float fromMax = 1.0;
     float toMin = 0.0;
     float toMax = 1.0;
-    float norm = map(absAcc * calcCoef, fromMin * calcCoef, fromMax * calcCoef,
-                     toMin * calcCoef, toMax * calcCoef) /
+    float norm = map(absAccAvg * calcCoef, fromMin * calcCoef,
+                     fromMax * calcCoef, toMin * calcCoef, toMax * calcCoef) /
                  calcCoef;
-    norm = constrain(norm, toMin, toMax);
-    // Serial.println(norm);
-    uint16_t freq = pow(1000, norm);
+    // norm = constrain(norm, toMin, toMax);
+    Serial.print(norm);
+    Serial.print(" ");
+    // uint16_t freq = pow(1000, norm);
+    uint16_t freq = 1000 * pow(norm, 2);
     Serial.println(freq);
 
-    M5.Beep.tone(freq, 100);
-    delay(100);
+    M5.Beep.tone(freq, interval / 1000);
   }
 }
