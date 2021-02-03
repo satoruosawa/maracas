de36826cad1ca119de4eb3199cbeb067c9849c2c
Satoru Osawa <osawa@takram.com>
2021-02-03 23:14:42 +0900

    Add beep

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,39 @@
-#include <Arduino.h>
+#include <M5StickCPlus.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  M5.begin();
+  M5.IMU.Init();
+  M5.Beep.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  M5.update();
+  M5.Beep.update();
+  static bool beepState = true;
+  if (M5.BtnA.wasPressed()) {
+    Serial.println("wasPressed");
+    beepState = !beepState;
+  }
+  if (beepState) {
+    float accX = 0.0;
+    float accY = 0.0;
+    float accZ = 0.0;
+    M5.IMU.getAccelData(&accX, &accY, &accZ);
+    float absAcc = abs(accX) + abs(accY) + abs(accZ);
+    float calcCoef = 1000.0;
+    float fromMin = 0.0;
+    float fromMax = 5.0;
+    float toMin = 0.0;
+    float toMax = 1.0;
+    float norm = map(absAcc * calcCoef, fromMin * calcCoef, fromMax * calcCoef,
+                     toMin * calcCoef, toMax * calcCoef) /
+                 calcCoef;
+    norm = constrain(norm, toMin, toMax);
+    // Serial.println(norm);
+    uint16_t freq = pow(1000, norm);
+    Serial.println(freq);
+
+    M5.Beep.tone(freq, 100);
+    delay(100);
+  }
 }
